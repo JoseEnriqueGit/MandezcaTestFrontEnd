bf3e59cf65cc74607a8c2a421585108727b284c8
Enrique.Git <75050597+JoseEnriqueGit@users.noreply.github.com>
2023-07-16 17:44:13 -0400

    Change: use react query

--- src/api/useFilterClientData.tsx
+++ src/api/useFilterClientData.tsx
@@ -1,61 +1,66 @@
 import { useEffect, useState } from "react";
-import axios, { AxiosResponse } from "axios";
+import { useQuery } from "react-query";
+import axios from "axios";
 import Fuse from "fuse.js";
 
 interface ClientData {
-  clientId: number;
-  clientName: string;
-  clientEmail: string;
-  clientPhone: string;
-  perfilId: number;
-  perfilTitle: string;
-  perfilDescription: string;
-  addressId: number;
-  addressLine: string;
-  city: string;
-  state: string;
-  country: string;
-  postalCode: string;
+	clientId: number;
+	clientName: string;
+	clientEmail: string;
+	clientPhone: string;
+	perfilId: number;
+	perfilTitle: string;
+	perfilDescription: string;
+	addressId: number;
+	addressLine: string;
+	city: string;
+	state: string;
+	country: string;
+	postalCode: string;
 }
 
 const options = {
-  threshold: 0.2,
-  keys: ["clientId" ,"clientName", "clientEmail"]
+	threshold: 0.2,
+	keys: ["clientId", "clientName", "clientEmail"],
 };
 
 const useFilterClientData = (toSearch: string, selectedPerfil: string) => {
-  const [allClientData, setAllClientData] = useState<ClientData[]>([]);
-  const [filteredClientData, setFilteredClientData] = useState<ClientData[]>([]);
+	const [filteredClientData, setFilteredClientData] = useState<ClientData[]>(
+		[]
+	);
 
-  useEffect(() => {
-    const fetchClientData = async () => {
-      try {
-        const response: AxiosResponse = await axios.get(
-          "https://localhost:7160/url/mnt/Client/all-data"
-        );
-        setAllClientData(response.data);
-      } catch (error) {
-        console.error("Error fetching client data:", error);
-      }
-    };
-    fetchClientData();
-  }, []);
+	const { data } = useQuery(
+		"clientData",
+		async () => {
+			const response = await axios.get<ClientData[]>(
+				"https://localhost:7160/url/mnt/Client/all-data"
+			);
+			return response.data;
+		},
+		{
+			refetchInterval: 100,
+		}
+	);
 
-  useEffect(() => {
-    const fuse = new Fuse(allClientData, options);
-    const filteredByName = toSearch
-      ? fuse.search(toSearch).map((result) => result.item)
-      : allClientData;
-    const filteredByPerfil =
-      selectedPerfil !== "Sin filtro"
-        ? filteredByName.filter((client) =>
-            client.perfilTitle.toLowerCase().includes(selectedPerfil.toLowerCase())
-          )
-        : filteredByName;
-    setFilteredClientData(filteredByPerfil);
-  }, [toSearch, selectedPerfil, allClientData]);
+	useEffect(() => {
+		if (data) {
+			const fuse = new Fuse(data, options);
+			const filteredByName = toSearch
+				? fuse.search(toSearch).map((result) => result.item)
+				: data;
+			const filteredByPerfil =
+				selectedPerfil !== "Sin filtro"
+					? filteredByName.filter((client) =>
+							client.perfilTitle
+								.toLowerCase()
+								.includes(selectedPerfil.toLowerCase())
+					  )
+					: filteredByName;
+			setFilteredClientData(filteredByPerfil);
+		}
+	}, [toSearch, selectedPerfil, data]);
 
-  return { filteredClientData };
+	return { filteredClientData };
 };
 
 export default useFilterClientData;
