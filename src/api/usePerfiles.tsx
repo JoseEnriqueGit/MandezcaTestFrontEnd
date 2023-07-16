6292262ce5ac9c180a39d2b4046a689fc9bd1fe3
Enrique.Git <75050597+JoseEnriqueGit@users.noreply.github.com>
2023-07-16 17:44:49 -0400

    Change: use react query

--- src/api/usePerfiles.tsx
+++ src/api/usePerfiles.tsx
@@ -1,40 +1,36 @@
-import { useEffect, useState } from "react";
+import { useQuery } from "react-query";
 import axios from "axios";
 
 type Profile = {
   perfilTitle: string;
 };
 
+const fetchPerfiles = async () => {
+  const response = await axios.get(
+    "https://localhost:7160/url/mnt/Client/all-data"
+  );
+  const data = response.data;
+
+  // Create a Set with explicit type annotation for the perfilTitle
+  const uniquePerfilesSet: Set<string> = new Set(
+    data.map((item: any) => item.perfilTitle)
+  );
+
+  // Convert the Set back to an array with explicit type annotation
+  const uniquePerfiles: Profile[] = Array.from(uniquePerfilesSet, (perfilTitle) => ({
+    perfilTitle,
+  }));
+
+  // Add "Sin filtro" as the first element in the array of perfiles
+  return [{ perfilTitle: "Sin filtro" }, ...uniquePerfiles];
+};
+
 const usePerfiles = () => {
-  const [perfiles, setPerfiles] = useState<Profile[]>([]);
+  const { data } = useQuery("perfiles", fetchPerfiles, {
+    refetchInterval: 100, // Refetch the data every 1 second
+  });
 
-  useEffect(() => {
-    const fetchPerfiles = async () => {
-      try {
-        const response = await axios.get(
-          "https://localhost:7160/url/mnt/Client/all-data"
-        );
-        const data = response.data;
-
-        // Create a Set with explicit type annotation for the perfilTitle
-        const uniquePerfilesSet: Set<string> = new Set(
-          data.map((item: any) => item.perfilTitle)
-        );
-
-        // Convert the Set back to an array with explicit type annotation
-        const uniquePerfiles: Profile[] = Array.from(uniquePerfilesSet, perfilTitle => ({ perfilTitle }));
-
-        // Add "Sin filtro" as the first element in the array of perfiles
-        setPerfiles([{ perfilTitle: "Sin filtro" }, ...uniquePerfiles]);
-      } catch (error) {
-        // Handle error if needed
-      }
-    };
-
-    fetchPerfiles();
-  }, []);
-
-  return perfiles;
+  return data || [];
 };
 
 export default usePerfiles;
