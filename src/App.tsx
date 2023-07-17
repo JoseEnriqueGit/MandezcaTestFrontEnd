d636e21e1e82ebe466f16b36b82bf20783a7496c
Enrique.Git <75050597+JoseEnriqueGit@users.noreply.github.com>
2023-07-17 07:59:10 -0400

    Change: use Routes, Route

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+// import { SearchSection, ClientDetails } from "./components";
+import { Routes, Route } from "react-router-dom";
+import { ViewDataPage, AddView } from "./Pages";
+import { EditView } from "./components";
 
 function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+	// const [isInforCardCliked, setisInforCardCliked] = useState(false);
+	const [clientCardSelected, setclientCardSelected] = useState(5);
+
+	return (
+		<main className="w-full max-w-2xl">
+			<Routes>
+				<Route index element={<ViewDataPage clientCardSelected={clientCardSelected} setclientCardSelected={setclientCardSelected} />} />
+				<Route path="/add" element={<AddView />} />
+				<Route path="/edit" element={<EditView id={clientCardSelected} />} />
+			</Routes>
+			{/* {!isInforCardCliked ? (
+				<SearchSection
+					setisInforCardCliked={setisInforCardCliked}
+					setclientCardSelected={setclientCardSelected}
+				/>
+			) : (
+				<ClientDetails setisInforCardCliked={setisInforCardCliked} id={clientCardSelected} />
+			)} */}
+		</main>
+	);
 }
 
-export default App
+export default App;
