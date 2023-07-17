c75762d4c566f3d680dd1b44e1cdeb6c85d183a6
Enrique.Git <75050597+JoseEnriqueGit@users.noreply.github.com>
2023-07-17 08:02:13 -0400

    New: FormData props

---
+++ src/components/EditData/Types/IFormData.ts
@@ -0,0 +1,15 @@
+interface FormData {
+  clientName?: string;
+  clientEmail?: string;
+  clientPhone?: string;
+  perfilTitle?: string;
+  perfilDescription?: string;
+  addressLine?: string;
+  city?: string;
+  state?: string;
+  country?: string;
+  postalCode?: string;
+}
+
+
+export type { FormData };
